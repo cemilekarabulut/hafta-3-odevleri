04428207bea1c77b9d7e401e52765da3e899a075
Cemile Karabulut <cemile.karabulut@metu.edu.tr>
2022-03-21 22:19:19 +0300

    Liste sınıfı ödevi tamamlandı.

--- Liste Sınıfı Ödevi/src/MyList.java
+++ Liste Sınıfı Ödevi/src/MyList.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class MyList<T> {
     private T[] list;
     private int capacity;
@@ -11,26 +15,116 @@ public class MyList<T> {
         this.capacity = capacity;
         this.list = (T[]) new Object[capacity];
     }
+
     public int size() {
         int size = 0;
-        for(T t: this.list){
-            if(t!=null) size++;
+        for (T t : this.list) {
+            if (t != null) size++;
         }
         return size;
     }
+
     public int getCapacity() {
         return this.capacity;
     }
+
     public void add(T data) {
-        if(this.capacity <= size()){
+        if (this.capacity <= size()) {
             this.capacity *= 2;
         }
         T[] newList = (T[]) new Object[capacity];
         int i = 0;
-        for(i = 0; i < size(); i++){
+        for (i = 0; i < size(); i++) {
             newList[i] = this.list[i];
         }
         newList[i] = data;
         this.list = newList.clone();
+    }
+
+    public T get(int index) {
+        if (index >= this.capacity && this.list[index] == null) {
+            return null;
+        }
+        return this.list[index];
+    }
+
+    public void remove(int index) {
+        if (index < this.capacity && this.list[index] != null) {
+            T[] newList = (T[]) new Object[capacity];
+            for (int i = 0; i < this.capacity; i++) {
+                if (i != index) {
+                    newList[i] = this.list[i];
+                }
+            }
+            this.list = newList;
+        }
+    }
+
+    public void set(int index, T data) {
+        if (index < this.capacity && this.list[index] != null) {
+            this.list[index] = data;
+        }
+    }
+
+    public int indexOf(T data) {
+        for (int i = 0; i < this.capacity; i++) {
+            if (this.list[i] == data)
+                return i;
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(T data) {
+        for (int i = this.capacity - 1; i >= 0; i--) {
+            if (this.list[i] == data)
+                return i;
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return this.capacity < 1 || this.list[0] == null;
+    }
+
+    public void clear() {
+        for (int i = 0; i < this.capacity; i++) {
+            if (this.list[i] != null)
+                this.remove(i);
+        }
+    }
+
+    public T[] toArray() {
+        ArrayList<T> temp = new ArrayList<>();
+        for (int i = 0; i < this.capacity; i++) {
+            if (this.list[i] != null)
+                temp.add(this.list[i]);
+        }
+        return (T[]) temp.toArray();
+    }
+
+    public MyList<T> sublist(int start, int finish) {
+        MyList<T> newList = new MyList<>();
+        ArrayList<T> temp = new ArrayList<>();
+        for (int i = 0; i < this.capacity; i++) {
+            if (i >= start && i <= finish)
+                temp.add(this.list[i]);
+        }
+        for (T t : temp) {
+            newList.add(t);
+        }
+        return newList;
+    }
+
+    public boolean contains(T data) {
+        for (T t : this.list) {
+            if (t != null && t.equals(data))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.stream(list).filter(Objects::nonNull).toArray());
     }
 }
--- Liste Sınıfı Ödevi/src/Parent.java
+++ Liste Sınıfı Ödevi/src/Parent.java
@@ -1,14 +1,15 @@
 public class Parent {
     public static void main(String[] args) {
         MyList<Integer> list = new MyList<>();
-        System.out.println("Dizideki Eleman Sayısı : " + list.size());
-        System.out.println("Dizinin Kapasitesi : " + list.getCapacity());
+        System.out.println("----------------- Part 1 -----------------");
+        System.out.println("Number of items in the array : " + list.size());
+        System.out.println("Array capacity : " + list.getCapacity());
         list.add(10);
         list.add(20);
         list.add(30);
         list.add(40);
-        System.out.println("Dizideki Eleman Sayısı : " + list.size());
-        System.out.println("Dizinin Kapasitesi : " + list.getCapacity());
+        System.out.println("Number of items in the array : " + list.size());
+        System.out.println("Array capacity : " + list.getCapacity());
         list.add(50);
         list.add(60);
         list.add(70);
@@ -16,7 +17,60 @@ public class Parent {
         list.add(90);
         list.add(100);
         list.add(110);
-        System.out.println("Dizideki Eleman Sayısı : " + list.size());
-        System.out.println("Dizinin Kapasitesi : " + list.getCapacity());
+        System.out.println("Number of items in the array : " + list.size());
+        System.out.println("Array capacity : " + list.getCapacity());
+
+        System.out.println("----------------- Part 2 -----------------");
+        list = new MyList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println("Value of 2. indice : " + list.get(2));
+        list.remove(2);
+        list.add(40);
+        list.set(0, 100);
+        System.out.println("Value of 2. indice : " + list.get(2));
+        System.out.println(list.toString());
+
+        System.out.println("----------------- Part 3 -----------------");
+        list = new MyList<>();
+        System.out.println("List State : " + (list.isEmpty() ? "Empty" : "Loaded"));
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(20);
+        list.add(50);
+        list.add(60);
+        list.add(70);
+
+        System.out.println("List State : " + (list.isEmpty() ? "Empty" : "Loaded"));
+
+        // The first index where given data is found
+        System.out.println("Indice : " + list.indexOf(20));
+
+        // If not found, returns -1
+        System.out.println("Indice :" + list.indexOf(100));
+
+        // The last index where given data is found
+        System.out.println("Indice : " + list.lastIndexOf(20));
+
+        // Returns a list as Object[]
+        Object[] objectList = list.toArray();
+        System.out.println("First element of object list :" + objectList[0]);
+
+        // Returns a sublist of type MyList
+        MyList<Integer> sublist = list.sublist(0, 3);
+        System.out.println(sublist.toString());
+
+        // Query for f the given value
+        System.out.println("The value of 20 in my list : " + list.contains(20));
+        System.out.println("The value of 120 in my list : " + list.contains(120));
+
+        // Clears the list
+        list.clear();
+        System.out.println(list.toString());
+
     }
+
 }
